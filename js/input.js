e047f7e9a662dd37771b861f5329fb89b0abf73e
vsrbl <vissarbel1811@gmail.com>
2026-05-17 02:54:38 +0300

    Refactor input handling by removing mouse events
    Removed mouse event handling from setupInput function.

--- js/input.js
+++ js/input.js
@@ -1,20 +1,19 @@
 export const input = {
+
     w:false,
     a:false,
     s:false,
-    d:false,
-    mouseX:0,
-    mouseY:0,
-    mouseDown:false
+    d:false
 };
 
-export function setupInput(canvas) {
+export function setupInput() {
 
     window.addEventListener('keydown', e => {
 
         const k = e.key.toLowerCase();
 
         if(input.hasOwnProperty(k)) {
+
             input[k] = true;
         }
     });
@@ -24,23 +23,8 @@ export function setupInput(canvas) {
         const k = e.key.toLowerCase();
 
         if(input.hasOwnProperty(k)) {
+
             input[k] = false;
         }
-    });
-
-    canvas.addEventListener('mousedown', () => {
-        input.mouseDown = true;
-    });
-
-    canvas.addEventListener('mouseup', () => {
-        input.mouseDown = false;
-    });
-
-    canvas.addEventListener('mousemove', e => {
-
-        const rect = canvas.getBoundingClientRect();
-
-        input.mouseX = e.clientX - rect.left;
-        input.mouseY = e.clientY - rect.top;
     });
 }
