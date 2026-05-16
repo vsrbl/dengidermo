cd37f6b3a8a564fa7728a27238996724685a76f9
vsrbl <vissarbel1811@gmail.com>
2026-05-17 02:45:23 +0300

    Refactor peer server configuration and allowed origins

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ const { PeerServer } = require('peer');
 
 const PORT = process.env.PORT || 9000;
 
-// Список разрешенных адресов (твои сайты)
-const ALLOWED_ORIGINS = [
-  'https://nncckkrr.space',  // Твой основной домен
-  'http://nncckkrr.space',   // На всякий случай (если зайдут без https)
-  'http://127.0.0.1:5500',   // Для тестов на твоем компе
-  'http://localhost:5500'    // Для тестов на твоем компе
-];
-
 const peerServer = PeerServer({
-  port: PORT,
-  path: '/myapp',
-  corsOptions: {
-    origin: ALLOWED_ORIGINS, // Пускаем только твой сайт!
-    methods: ['GET', 'POST']
-  }
+    port: PORT,
+    path: '/myapp',
+    proxied: true,
+    corsOptions: {
+        origin: [
+            'https://nncckkrr.space',
+            'http://localhost:5500',
+            'http://127.0.0.1:5500'
+        ],
+        methods: ['GET', 'POST']
+    }
 });
 
-console.log(`Сигнальный сервер запущен на порту ${PORT}`);
-console.log(`Доступ разрешен для:`, ALLOWED_ORIGINS);
+console.log(`PeerJS server started on ${PORT}`);
