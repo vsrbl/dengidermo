8e7705caed2f425f003e14f75651c6e3c4c5b259
vsrbl <vissarbel1811@gmail.com>
2026-05-17 02:31:14 +0300

    Add game constants for player and bullet settings

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const NET_TICK = 1000 / 20;
+
+export const PLAYER_SPEED = 4;
+
+export const PLAYER_SIZE = 10;
+
+export const BULLET_SPEED = 14;
+
+export const MAX_PLAYERS = 4;
+
+export const FIRE_RATE = 120;
